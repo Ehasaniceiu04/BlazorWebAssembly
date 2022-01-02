e3f2756c1717eb86c700e49f68d870abf165b86d
Ehasanul Hoque <ehasanul@julyservices.com>
2022-01-02 12:32:56 +0600

    first commit

---
+++ Blazor.Wasm.UI/Models/CustomerModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Blazor.Wasm.UI.Models
+{
+    public class CustomerModel
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public int GenderId { get; set; }
+        public int CustomerId { get; set; }
+        public string MailingAddress { get; set; }
+        public bool IsAgreeToGetPromotionalEmail { get; set; }
+    }
+}
---
+++ Blazor.Wasm.UI/Pages/Customer.razor.cs
@@ -0,0 +1,22 @@
+﻿using Blazor.Wasm.UI.Models;
+
+namespace Blazor.Wasm.UI.Pages
+{
+    public partial class Customer
+    {
+        CustomerModel model = new CustomerModel();
+
+        List<GenderModel> genders = new List<GenderModel>() {
+            new GenderModel(){Id = 1, Name ="Male"},
+             new GenderModel(){Id = 2, Name ="Female"},
+              new GenderModel(){Id = 3, Name ="Other"},
+        };
+
+        List<CountryModel> countries = new List<CountryModel>() { 
+            new CountryModel(){Id = 1, Name ="USA" },
+            new CountryModel(){Id = 2, Name ="Bangladesh"},
+             new CountryModel(){Id = 3, Name ="Nepal"},
+             new CountryModel(){Id = 4, Name ="India"}
+        };
+    }
+}
