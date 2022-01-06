bd3190716c892113a688c11f3345326725280994
Ehasanul Hoque <ehasanul@julyservices.com>
2022-01-07 02:50:41 +0600

    added checkbox

--- Blazor.Wasm.UI/Models/CustomerModel.cs
+++ Blazor.Wasm.UI/Models/CustomerModel.cs
@@ -9,6 +9,9 @@
         public int GenderId { get; set; }
         public int CustomerId { get; set; }
         public string MailingAddress { get; set; }
+
         public bool IsAgreeToGetPromotionalEmail { get; set; }
+
+
     }
 }
