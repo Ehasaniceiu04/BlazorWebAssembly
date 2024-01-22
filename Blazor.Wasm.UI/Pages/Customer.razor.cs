52a4223adb8404563416c1d0d0ea1a0d25cb3e74
Ehasaniceiu04 <ehasan@outlook.com>
2024-01-23 00:28:20 +0600

    customers added

---
+++ Blazor.Wasm.UI/Pages/Customer.razor.cs
@@ -0,0 +1,24 @@
+﻿using Blazor.Wasm.UI.Models;
+using Microsoft.AspNetCore.Components;
+using System.Net.Http.Json;
+
+
+namespace Blazor.Wasm.UI.Pages
+{
+    public partial class Customer : ComponentBase
+    {
+        List<CustomerModel> customers = new List<CustomerModel>();
+
+        [Inject]
+        private HttpClient Http { get; set; }
+        public Customer()
+        {
+
+        }
+        protected override async Task OnInitializedAsync()
+        {
+            this.customers = await Http.GetFromJsonAsync<List<CustomerModel>>("api/Customer");
+        }
+
+    }
+}
