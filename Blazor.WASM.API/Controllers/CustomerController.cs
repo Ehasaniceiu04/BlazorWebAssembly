dd87f031144789c37fca9543caf97faafc01c4a6
Ehasaniceiu04 <ehasan@outlook.com>
2024-01-26 00:25:05 +0600

    add update and get by id api

--- Blazor.WASM.API/Controllers/CustomerController.cs
+++ Blazor.WASM.API/Controllers/CustomerController.cs
@@ -20,8 +20,22 @@ namespace Blazor.WASM.API.Controllers
             var customers = await _context.Customers.ToListAsync();
             return Ok(customers);
         }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetByIdAsync(int id)
+        {
+            var customers = await _context.Customers.FirstOrDefaultAsync(x=>x.Id==id);
+            return Ok(customers);
+        }
         [HttpPost]
         public async Task<IActionResult> Create([FromBody] Customer model)
+        {
+            await _context.Customers.AddAsync(model);
+            await _context.SaveChangesAsync();
+            return Ok(model);
+        }
+        
+        [HttpPut]
+        public async Task<IActionResult> Update([FromBody] Customer model)
         {
             await _context.Customers.AddAsync(model);
             await _context.SaveChangesAsync();
