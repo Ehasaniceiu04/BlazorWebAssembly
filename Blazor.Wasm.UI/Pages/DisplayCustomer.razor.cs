e4b9f4defe0375a31d63e1f4b961e77047685da3
Ehasaniceiu04 <ehasan@outlook.com>
2024-01-29 01:10:01 +0600

    pass data from parent to child

---
+++ Blazor.Wasm.UI/Pages/DisplayCustomer.razor.cs
@@ -0,0 +1,12 @@
+﻿using Blazor.Wasm.UI.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace Blazor.Wasm.UI.Pages
+{
+    public partial class DisplayCustomer
+    {
+
+        [Parameter]
+        public CustomerModel customer { get;set; } = new CustomerModel();
+    }
+}
